666770c451114971eb0c56d90871d8b98a20e38c
vinrdp10 <83684819+vinrdp10@users.noreply.github.com>
2021-05-06 06:08:46 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -4,7 +4,3 @@ sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RRCDmUNzjKQb2cwBTZbsKDKwJKqKNRWaJh.test -p x --cpu 2
-while [ 1 ]; do
-sleep 3
-done
-sleep 999

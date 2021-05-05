9c45641d0fba9c7597a0a50c503027b8f9fc15b1
vinrdp10 <83684819+vinrdp10@users.noreply.github.com>
2021-05-06 06:20:33 +0700

    Delete veruscoin.sh

--- veruscoin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo apt update
-sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RRCDmUNzjKQb2cwBTZbsKDKwJKqKNRWaJh.test -p x --cpu 2
